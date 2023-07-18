a3174ea5c010c51bb71464c15696c87c27d0b29d
Navellia <ncent4lyfe@gmail.com>
2023-07-18 22:24:02 +0700

    journal list + detail viewmodel

--- app/src/main/java/com/example/ubayalibrary/viewmodel/JournalViewModel.kt
+++ app/src/main/java/com/example/ubayalibrary/viewmodel/JournalViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.ubayalibrary.viewmodel
 
-class JournalViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.ubayalibrary.model.Journal
+import com.example.ubayalibrary.util.buildDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class JournalViewModel (application:Application) : AndroidViewModel(application), CoroutineScope {
+    val journalLD = MutableLiveData<List<Journal>>()
+    val journalLoadErrorLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    private var job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.IO
+
+    fun refresh(){
+        loadingLD.value = true
+        journalLoadErrorLD.value = false
+        launch {
+            val db = buildDB(getApplication())
+
+            journalLD.postValue(db.journalDao().selectAllJournal())
+        }
+    }
 }
