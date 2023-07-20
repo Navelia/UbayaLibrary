829ff04c40f09250c925df05b2afdf259372c833
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-07-20 22:14:14 +0700

    notif revisi

--- app/src/main/java/com/example/ubayalibrary/util/NotificationHelper.kt
+++ app/src/main/java/com/example/ubayalibrary/util/NotificationHelper.kt
@@ -51,13 +51,6 @@ class NotificationHelper(val context: Context) {
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return
         }
         NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
